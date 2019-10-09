c4a77d10899ff8b04f71da102fbd076c738567d9
Ira <irridee@gmail.com>
2019-10-10 00:28:36 +0300

    Add lesson.py

---
+++ Lesson4.py
@@ -0,0 +1,14 @@
+print(1 is 1)
+a=30000
+b=30000
+print(a is b)
+print("1" is "1")
+print("1" is 1)
+print(1 is 1.0)
+print(1==1.0)
+print(1==True)
+print(1 is True)
+a=5
+a+=1
+print(a)
+print("Ira")
